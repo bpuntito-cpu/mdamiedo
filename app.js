1ed8d5ddd931ea7620d197a83dc026b7f04dfd45
bpuntito-cpu <b.puntitoo@gmail.com>
2026-03-09 16:51:01 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const boton = document.getElementById("btnEnviar");
+
+boton.addEventListener("click", function(){
+
+const texto = document.getElementById("inputFrase").value;
+
+console.log(texto);
+
+});
